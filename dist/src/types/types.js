d68c5fdd5afa2799425b468ad2c2af9151e5eb81
Erez Liberman <erez.lieberman@ridewithvia.com>
2022-10-24 21:07:58 +0300

    ignore dist + add streams

--- dist/src/types/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InputTypes = void 0;
-var InputTypes;
-(function (InputTypes) {
-    InputTypes["FILE"] = "file";
-    InputTypes["URL"] = "url";
-    InputTypes["STRING"] = "string";
-})(InputTypes = exports.InputTypes || (exports.InputTypes = {}));
